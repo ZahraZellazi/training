1f7af0917023e8c592c86f6621b8a013287a6f12
ZahraZellazi <zahra.zellazi@esprit.tn>
2024-07-22 23:13:15 +0100

    express

---
+++ HttpServer/server.js
@@ -0,0 +1,13 @@
+const http = require ('http')
+const url = require ("url")
+//declare the server 
+
+const server = http.createServer((request,res)=>{
+   //conditions
+})
+ 
+// run the server 
+
+server.listen(8000, () =>{
+    console.log("server is running")
+})
