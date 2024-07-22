835d8623f4edf18ff3b77770c430f0dbe605a7d2
ZahraZellazi <zahra.zellazi@esprit.tn>
2024-07-22 23:45:05 +0100

    server_part1

--- HttpServer/server.js
+++ HttpServer/server.js
@@ -1,13 +1,24 @@
-const http = require ('http')
-const url = require ("url")
-//declare the server 
+const http = require('http');
+const url = require('url');
 
-const server = http.createServer((request,res)=>{
-   //conditions
-})
- 
-// run the server 
+var list = [{ name: 'Zahra', age: 77 }];
 
-server.listen(8000, () =>{
-    console.log("server is running")
-})
+// declare the server
+const server = http.createServer((request, res) => {
+  const parseUrl = url.parse(request.url, true);
+  const path = parseUrl.pathname;
+  const method = request.method;
+  
+  if (path === "/items" && method === "GET") {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(list));
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+});
+
+// run the server
+server.listen(8000, () => {
+  console.log('server is running');
+});
